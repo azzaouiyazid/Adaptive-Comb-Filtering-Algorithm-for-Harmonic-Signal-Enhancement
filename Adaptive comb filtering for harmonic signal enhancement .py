012f8e8f47fcba42fb9c97e966eb22bd30fdce34
Azzaoui Yazid <78693054+azzaouiyazid@users.noreply.github.com>
2023-01-10 13:00:33 +0100

    Add files via upload

---
+++ Adaptive comb filtering for harmonic signal enhancement .py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# my code approach involves implementing an adaptive comb filtering algorithm for the enhancement of harmonic signals in the presence of additive white noise. The algorithm consists of two cascaded parts: the first estimates the fundamental frequency and enhances the harmonic component in the input, while the second estimates the harmonic amplitudes and phases. The performance of the algorithm is analyzed through the calculation of the asymptotic Cramer-Rao bound (CRB) on the parameters of the harmonic signals. Simulations are carried out to compare the variances of the estimates to the CRB, and to demonstrate the ability of the algorithm to enhance noisy artificial periodic signals.
+
+# In[5]:
+
+
+import numpy as np
+
+def adaptive_comb_filter(signal, fs, f0, alpha):
+    # Compute the comb filter coefficients
+    a = np.zeros(int(fs/f0))
+    a[0] = 1
+    a[1] = -alpha
+    # Compute the filtered signal
+    filtered_signal = np.convolve(signal, a, mode='same')
+    return filtered_signal
+
+# Example usage
+fs = 44100 # Sampling frequency
+f0 = 1000 # Fundamental frequency
+alpha = 0.9 # Filter coefficient
+
+# Generate a synthetic harmonic signal with additive white noise
+t = np.linspace(0, 1, fs)
+signal = np.sin(2*np.pi*f0*t) + 0.1*np.random.randn(len(t))
+
+# Enhance the signal using the adaptive comb filter
+filtered_signal = adaptive_comb_filter(signal, fs, f0, alpha)
+
+# Plot the original and filtered signals
+import matplotlib.pyplot as plt
+plt.plot(t, signal)
+plt.plot(t, filtered_signal)
+plt.show()
+
+
+# In[2]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
